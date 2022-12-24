739e56ff942f5e240374993570d1d2bbd9a92b88
ThinkJ <ledinhthinh.ws@gmail.com>
2022-12-25 00:23:24 +0700

    commit

---
+++ src/main/java/beans/tools/Compa.java
@@ -0,0 +1,11 @@
+package beans;
+
+public class Compa {
+    private String name = "Compa";
+
+    public void drawCircle()
+    {
+        System.out.println("Name: "+name);
+        System.out.println("Draw circle successfully");
+    }
+}
---
+++ src/main/java/beans/tools/Ruler.java
@@ -0,0 +1,2 @@
+package beans.tools;public class Ruler {
+}
